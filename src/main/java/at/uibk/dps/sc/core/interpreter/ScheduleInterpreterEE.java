890cd93c60535e361c828dced1d06fef93b698b5
Fedor <fedor@dps.uibk.ac.at>
2021-03-17 08:45:51 +0100

    factories for function creation

--- src/main/java/at/uibk/dps/sc/core/interpreter/ScheduleInterpreterEE.java
+++ src/main/java/at/uibk/dps/sc/core/interpreter/ScheduleInterpreterEE.java
@@ -1,22 +1,12 @@
 package at.uibk.dps.sc.core.interpreter;
 
 import java.util.Set;
-
+import com.google.inject.Inject;
 import at.uibk.dps.ee.core.enactable.EnactmentFunction;
-import at.uibk.dps.ee.enactables.local.dataflow.Aggregation;
-import at.uibk.dps.ee.enactables.local.dataflow.Distribution;
-import at.uibk.dps.ee.enactables.local.dataflow.EarliestArrival;
-import at.uibk.dps.ee.enactables.local.dataflow.Multiplexer;
-import at.uibk.dps.ee.enactables.local.utility.CollOperFunction;
-import at.uibk.dps.ee.enactables.local.utility.ConditionEvaluation;
+import at.uibk.dps.ee.enactables.local.dataflow.FunctionFactoryDataFlow;
+import at.uibk.dps.ee.enactables.local.utility.FunctionFactoryUtility;
 import at.uibk.dps.ee.model.properties.PropertyServiceFunction;
 import at.uibk.dps.ee.model.properties.PropertyServiceFunction.UsageType;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlow;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlow.DataFlowType;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlowCollections;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlowCollections.OperationType;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUtility;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUtility.UtilityType;
 import net.sf.opendse.model.Mapping;
 import net.sf.opendse.model.Resource;
 import net.sf.opendse.model.Task;
@@ -30,6 +20,16 @@ import net.sf.opendse.model.Task;
  */
 public class ScheduleInterpreterEE implements ScheduleInterpreter {
 
+  protected final FunctionFactoryDataFlow factoryDataFlow;
+  protected final FunctionFactoryUtility factoryUtility;
+  
+  
+  @Inject
+  public ScheduleInterpreterEE(FunctionFactoryDataFlow factoryDataFlow, FunctionFactoryUtility factoryUtility) {
+    this.factoryDataFlow = factoryDataFlow;
+    this.factoryUtility = factoryUtility;
+  }
+
   @Override
   public EnactmentFunction interpretSchedule(final Task task,
       final Set<Mapping<Task, Resource>> scheduleModel) {
@@ -38,54 +38,11 @@ public class ScheduleInterpreterEE implements ScheduleInterpreter {
     }
     final UsageType usage = PropertyServiceFunction.getUsageType(task);
     if (usage.equals(UsageType.DataFlow)) {
-      return getDataFlowFunction(task);
+      return factoryDataFlow.getDataFlowFunction(task);
     } else if (usage.equals(UsageType.Utility)) {
-      return getUtilityFunction(task);
+      return factoryUtility.getUtilityFunction(task);
     } else {
       throw new IllegalArgumentException("Neither a utility nor a data flow task: " + task.getId());
-    }
-  }
-
-  /**
-   * Returns the data flow function for the given task.
-   * 
-   * @param task the given task.
-   * @return the data flow function for the given task
-   */
-  protected EnactmentFunction getDataFlowFunction(final Task task) {
-    final DataFlowType dfType = PropertyServiceFunctionDataFlow.getDataFlowType(task);
-    if (dfType.equals(DataFlowType.EarliestInput)) {
-      return new EarliestArrival();
-    }else if (dfType.equals(DataFlowType.Multiplexer)) {
-      return new Multiplexer();
-    }else if (dfType.equals(DataFlowType.Collections)) {
-      final OperationType oType = PropertyServiceFunctionDataFlowCollections.getOperationType(task);
-      if (oType.equals(OperationType.Aggregation)) {
-        return new Aggregation();
-      } else if (oType.equals(OperationType.Distribution)) {
-        return new Distribution(task);
-      } else {
-        throw new IllegalArgumentException("Unknown coll operation type: " + oType.name());
-      }
-    } else {
-      throw new IllegalArgumentException("Unknown data flow type: " + dfType.name());
-    }
-  }
-
-  /**
-   * Returns the utility function for the given task.
-   * 
-   * @param task the given task.
-   * @return the utility function for the given task
-   */
-  protected EnactmentFunction getUtilityFunction(final Task task) {
-    final UtilityType utilType = PropertyServiceFunctionUtility.getUtilityType(task);
-    if (utilType.equals(UtilityType.Condition)) {
-      return new ConditionEvaluation(task);
-    } else if (utilType.equals(UtilityType.CollectionOperation)) {
-      return new CollOperFunction(task);
-    } else {
-      throw new IllegalArgumentException("Unknown utility type: " + utilType.name());
     }
   }
 }
--- src/test/java/at/uibk/dps/sc/core/interpreter/ScheduleInterpreterEETest.java
+++ src/test/java/at/uibk/dps/sc/core/interpreter/ScheduleInterpreterEETest.java
@@ -3,48 +3,40 @@ package at.uibk.dps.sc.core.interpreter;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import at.uibk.dps.ee.core.enactable.EnactmentFunction;
-import at.uibk.dps.ee.enactables.local.dataflow.Aggregation;
-import at.uibk.dps.ee.enactables.local.dataflow.Distribution;
-import at.uibk.dps.ee.enactables.local.dataflow.EarliestArrival;
-import at.uibk.dps.ee.enactables.local.dataflow.Multiplexer;
-import at.uibk.dps.ee.enactables.local.utility.CollOperFunction;
-import at.uibk.dps.ee.enactables.local.utility.ConditionEvaluation;
-import at.uibk.dps.ee.model.objects.Condition;
+import at.uibk.dps.ee.enactables.local.dataflow.FunctionFactoryDataFlow;
+import at.uibk.dps.ee.enactables.local.utility.FunctionFactoryUtility;
 import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlow;
 import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlow.DataFlowType;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlowCollections.OperationType;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlowCollections;
 import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUtilityCollections.CollectionOperation;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUtilityCondition;
 import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUtilityCollections;
 import net.sf.opendse.model.Mapping;
 import net.sf.opendse.model.Resource;
 import net.sf.opendse.model.Task;
-import static org.mockito.Mockito.spy;
-import java.util.ArrayList;
+import static org.mockito.Mockito.when;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import static org.mockito.Mockito.doReturn;
-
 import static org.mockito.Mockito.mock;
 
 public class ScheduleInterpreterEETest {
 
   @Test
   public void testInterpretSchedule() {
-    ScheduleInterpreterEE tested = new ScheduleInterpreterEE();
-    ScheduleInterpreterEE spy = spy(tested);
+    
+    FunctionFactoryUtility utilityMock = mock(FunctionFactoryUtility.class);
+    FunctionFactoryDataFlow dataFlowMock = mock(FunctionFactoryDataFlow.class);
     Task dfTask = PropertyServiceFunctionDataFlow.createDataFlowFunction("taskdf",
         DataFlowType.EarliestInput);
     Task utilTask = PropertyServiceFunctionUtilityCollections.createCollectionOperation("bla",
         "blabla", CollectionOperation.Block);
     EnactmentFunction dataFunc = mock(EnactmentFunction.class);
     EnactmentFunction utilFunc = mock(EnactmentFunction.class);
-    doReturn(dataFunc).when(spy).getDataFlowFunction(dfTask);
-    doReturn(utilFunc).when(spy).getUtilityFunction(utilTask);
-    assertEquals(dataFunc, spy.interpretSchedule(dfTask, new HashSet<>()));
-    assertEquals(utilFunc, spy.interpretSchedule(utilTask, new HashSet<>()));
+    when(utilityMock.getUtilityFunction(utilTask)).thenReturn(utilFunc);
+    when(dataFlowMock.getDataFlowFunction(dfTask)).thenReturn(dataFunc);
+    
+    ScheduleInterpreterEE tested = new ScheduleInterpreterEE(dataFlowMock, utilityMock);
+    
+    assertEquals(dataFunc, tested.interpretSchedule(dfTask, new HashSet<>()));
+    assertEquals(utilFunc, tested.interpretSchedule(utilTask, new HashSet<>()));
 
     Mapping<Task, Resource> mapping = new Mapping<>("mapping", utilTask, new Resource("res"));
     Set<Mapping<Task, Resource>> mappings = new HashSet<>();
@@ -54,34 +46,5 @@ public class ScheduleInterpreterEETest {
       fail();
     } catch (IllegalArgumentException exc) {
     }
-  }
-
-  @Test
-  public void testGetDataFlowTask() {
-    ScheduleInterpreterEE tested = new ScheduleInterpreterEE();
-    Task earliestInTask =
-        PropertyServiceFunctionDataFlow.createDataFlowFunction("t", DataFlowType.EarliestInput);
-    Task aggrTask = PropertyServiceFunctionDataFlowCollections.createCollectionDataFlowTask("t1",
-        OperationType.Aggregation, "scope");
-    Task distTask = PropertyServiceFunctionDataFlowCollections.createCollectionDataFlowTask("t1",
-        OperationType.Distribution, "scope");
-    Task muxerTask =
-        PropertyServiceFunctionDataFlow.createDataFlowFunction("t1", DataFlowType.Multiplexer);
-    assertTrue(tested.getDataFlowFunction(earliestInTask) instanceof EarliestArrival);
-    assertTrue(tested.getDataFlowFunction(distTask) instanceof Distribution);
-    assertTrue(tested.getDataFlowFunction(aggrTask) instanceof Aggregation);
-    assertTrue(tested.getDataFlowFunction(muxerTask) instanceof Multiplexer);
-  }
-
-  @Test
-  public void testGetUtilityFunction() {
-    ScheduleInterpreterEE tested = new ScheduleInterpreterEE();
-    List<Condition> conditions = new ArrayList<>();
-    Task conditionTask =
-        PropertyServiceFunctionUtilityCondition.createConditionEvaluation("bla", conditions);
-    Task collectionTask = PropertyServiceFunctionUtilityCollections.createCollectionOperation("bla",
-        "blabla", CollectionOperation.Block);
-    assertTrue(tested.getUtilityFunction(collectionTask) instanceof CollOperFunction);
-    assertTrue(tested.getUtilityFunction(conditionTask) instanceof ConditionEvaluation);
   }
 }
